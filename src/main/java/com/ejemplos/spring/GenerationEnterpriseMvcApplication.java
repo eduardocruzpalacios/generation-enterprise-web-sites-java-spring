08583adaa5fc2dee76c3b0c2ddd5a380ec44dad2
educp <52535457+eduardocruzpalacios@users.noreply.github.com>
2021-05-11 10:21:47 +0200

    inicio proyecto, dependencias Swagger y Springfox

---
+++ src/main/java/com/ejemplos/spring/GenerationEnterpriseMvcApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplos.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationEnterpriseMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationEnterpriseMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplos/spring/GenerationEnterpriseMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplos.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationEnterpriseMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
