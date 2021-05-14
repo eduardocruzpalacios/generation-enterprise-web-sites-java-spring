874ce19aaa8bd0ba54ad1160934fde88dedf4569
unknown <80522547+Arrshe@users.noreply.github.com>
2021-05-14 12:39:50 +0200

    puestas anotaciones en el service de cargo y persona

--- src/main/java/com/ejemplos/spring/service/CargoServiceImpl.java
+++ src/main/java/com/ejemplos/spring/service/CargoServiceImpl.java
@@ -15,10 +15,12 @@ package com.ejemplos.spring.service;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.ejemplos.spring.model.Cargo;
 
+@Service
 public class CargoServiceImpl implements CargoService {
 
 	// Metodo para listar
